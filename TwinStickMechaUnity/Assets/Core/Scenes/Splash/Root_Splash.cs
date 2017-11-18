60d96cbee08191378ce43e2a928149460baf44c9
Jake <jake.a.oconnor@gmail.com>
2017-11-18 07:58:20 -0800

    TMP + Scene Scaffold

---
+++ TwinStickMechaUnity/Assets/Core/Scenes/Splash/Root_Splash.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Prime31.TransitionKit;
+using UnityEngine.SceneManagement;
+
+public class Root_Splash : MonoBehaviour
+{
+    private IEnumerator Start()
+    {
+        SceneManager.LoadSceneAsync(k.Scenes.SCENE_HARNESS, LoadSceneMode.Additive);
+
+        yield return new WaitForSeconds(5f);
+
+        TransitionKit.instance.transitionWithDelegate(new TriangleSlicesTransition()
+        {
+            duration = 2f,
+            divisions = 7,
+            sceneLoadData = new SceneLoadData()
+            {
+                LoadIndex = k.Scenes.SCENE_MAIN_MENU_IDX,
+                UnloadIndex = k.Scenes.SCENE_SPLASH_IDX,
+                LoadMode = LoadSceneMode.Additive,
+                LoadOrder = SceneLoadData.Order.UnloadFirst
+            }
+        });
+    }
+}
