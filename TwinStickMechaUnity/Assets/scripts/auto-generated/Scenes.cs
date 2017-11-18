f813d9cf698b87e395c315f9e78a2d07a10cb535
Jake <jake.a.oconnor@gmail.com>
2017-11-18 07:54:27 -0800

    Fold Structure Commit
    Initial folder struture and Unity project commit.

---
+++ TwinStickMechaUnity/Assets/scripts/auto-generated/Scenes.cs
@@ -0,0 +1,24 @@
+//This class is auto-generated do not modify
+namespace k
+{
+	public static class Scenes
+	{
+		public const string SCENE_SPLASH = "Scene_Splash";
+		public const string SCENE_HARNESS = "Scene_Harness";
+		public const string SCENE_MAIN_MENU = "Scene_MainMenu";
+		public const string SCENE_GARAGE = "Scene_Garage";
+		public const string SCENE_SHOP = "Scene_Shop";
+		public const string SCENE_PRE_GAME = "Scene_PreGame";
+		public const string SCENE_IN_GAME = "Scene_InGame";
+		public const string SCENE_POST_GAME = "Scene_PostGame";
+
+		public const int SCENE_SPLASH_IDX = 0;
+		public const int SCENE_HARNESS_IDX = 1;
+		public const int SCENE_MAIN_MENU_IDX = 2;
+		public const int SCENE_GARAGE_IDX = 3;
+		public const int SCENE_SHOP_IDX = 4;
+		public const int SCENE_PRE_GAME_IDX = 5;
+		public const int SCENE_IN_GAME_IDX = 6;
+		public const int SCENE_POST_GAME_IDX = 7;
+	}
+}
